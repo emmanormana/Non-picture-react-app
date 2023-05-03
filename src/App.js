133c3f92b117543d700b85c1e6381e357cb8a102
emmanormana <emmanorman891@gmail.com>
2023-05-02 21:41:13 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+// import the Google Maps API Wrapper from google-maps-react
+import { GoogleApiWrapper } from 'google-maps-react' 
+// import child component
+import MapContainer from './MapContainer'
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Nokia Open Source Smart City project | Powered by Emma Norman </h1> 
+
+        <MapContainer google={this.props.google} />
+      </div>
+    );
+  }
+}
+// OTHER MOST IMPORTANT: Here we are exporting the App component WITH the GoogleApiWrapper. You pass it down with an object containing your API key
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCPvegTVYEK30o7eLBed8y1ikDlPpW0wr4',
+})(App)
---
+++ src/MapContainer.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+export default class MapContainer extends Component {
+
+  // ======================
+  // ADD LOCATIONS TO STATE
+  // ======================
+  
+ state = {
+    location: []
+  }
+ 
+  componentDidUpdate() {
+      this.loadMap()   
+  }
+
+
+//This function fetches data from backend and stores in state object
+  fetchData() {
+    axios.get("http://localhost:3001")
+      .then( (response) => {
+        const location =response.data;
+        this.setState({ location });
+      })
+      .catch( (error) => { 
+      console.log(error);
+      });
+  }
+
+
+//This function will be called initially
+componentWillMount(){
+    this.fetchData()
+  }
+
+  loadMap() {
+    if (this.props && this.props.google) { // checks to make sure that props have been passed
+      const {google} = this.props; // sets props equal to google
+      const maps = google.maps; // sets maps to google maps props
+
+      const mapRef = this.refs.map; // looks for HTML div ref 'map'. Returned in render below.
+      const node = ReactDOM.findDOMNode(mapRef); // finds the 'map' div in the React DOM, names it node
+
+      const mapConfig = Object.assign({}, {
+        center: {lat: 61.497978, lng: 23.764931}, // sets center of google map to Tampere City.
+        zoom: 11, // sets zoom. Lower numbers are zoomed further out.
+        mapTypeId: 'roadmap' // optional main map layer. Terrain, satellite, hybrid or roadmap--if unspecified, defaults to roadmap.
+      })
+
+      this.map = new maps.Map(node, mapConfig); // creates a new Google map on the specified node (ref='map') with the specified configuration set above.
+
+  // ==================
+  // ADD MARKERS TO MAP
+  // ==================
+      this.state.location.forEach( location => { // iterate through locations saved in state
+        const marker = new google.maps.Marker({ // creates a new Google maps Marker object.
+          position: {lat: parseFloat(location.latitude.value), lng: parseFloat(location.longitude.value)}, // sets position of marker to specified location
+          map: this.map, // sets markers to appear on the map we just created on line 35
+          title: location.id // the title of the marker is set to the name of the location
+        });
+
+         var infowindow = new google.maps.InfoWindow({
+          content: `<h3>ID: ${location.id}</h3>
+                    <h4>Humidity: ${location.humidity.value}</h4>
+                    <h4>Temperature: ${location.temperature.value}</h4>`
+        });
+          marker.addListener('click', function() {
+          infowindow.open(this.map, marker);
+        });
+
+      })
+
+
+
+     
+    }
+  }
+
+  render() {
+    const style = { // MUST specify dimensions of the Google map or it will not work. Also works best when style is specified inside the render function and created as an object
+      width: '100vw', // 100vw basically means take up 90% of the width screen. px also works.
+      height: '75vh' // 75vh similarly will take up roughly 75% of the height of the screen. px also works.
+    }
+
+    return ( // in our return function you must return a div with ref='map' and style.
+      <div ref="map" style={style}>
+        loading map...
+      </div>
+    )
+  }
+}
